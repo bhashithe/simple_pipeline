500666a3e7deb069be5538d510f239e7e52af08c
Bhashithe Abeysinghe <bhashithe@gmail.com>
2018-10-08 10:30:19 -0400

    added generate_parameters

--- Pipeline.py
+++ Pipeline.py
@@ -7,11 +7,11 @@ class Pipeline():
 	def arguments(self):
 		parser = argparse.ArgumentParser(description='PyTorch NN training pipeline')
 		parser.add_argument('parameters', help='Python file to use in the operation mode', type=str)
-		parser.add_argument('trainer', help='Extended trainer class', type=str)
-		parser.add_argument('--generate', help='generate parameters file with defaults')
+		parser.add_argument('--trainer', help='Extended trainer class', type=str)
+		parser.add_argument('--generate', help='Generate a parameters file or a model file by using parameter, or model as the value for this')
 		
 		args = parser.parse_args()
-		return args.parameters,args.trainer, args.generate
+		return args.parameters,args.trainer, args.generate, args.model
 
 	def generate_model(self, filename='Model.py'):
 		pass
@@ -25,11 +25,53 @@ class Pipeline():
 		Arguments:
 			filename: path and the name of the file, extention should be .py
 		"""
-		pass
+		
+		content = """\
+import torch
+from torch.optim import Adam
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import DataLoader, Dataset, random_split
+from torchvision import transforms
+
+model = #Add nn.Module implemented classifier
+
+trns = # torchvision.transfomrs you want to add if there is any
+
+dataset_size = {'train': 7000, 'val': 2144} # add your own sizes
+dataset = # load torch.util.data.Dataset type data here so that you have access to random_split and DataLoaders
+batch_size = 20
+data = {}
+data['train'], data['val'] = random_split(dataset, (dataset_size['train'], dataset_size['val']))
+loader = {phase: DataLoader(data[phase],batch_size=batch_size) for phase in ['train','val']}
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+def training_parameters():
+	parameters = {
+		'model': model,
+		'optimizer': Adam(model.parameters()),
+		'scheduler': StepLR(optimizer, step_size=7),
+		'loader': loader,
+		'loss_function': torch.nn.CrossEntropyLoss(),
+		'device': device,
+		'epochs': 25
+	}
+
+	return parameters
+
+if __name__ == '__main__':
+	training_parameters()
+"""
+
+		with open(filename+'.py', 'w') as f:
+			f.write(content)
+
+		print('file written')
+		return True
 
 def main():
 	p = Pipeline()
-	parameters, trainer, generate = p.arguments()
+	parameters, trainer, generate, model = p.arguments()
 
 	if generate == 'parameters':
 		p.generate_parameters(parameters)
